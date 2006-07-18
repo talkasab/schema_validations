666da709714f6af19c6c91cacd8aac508a696a65
haruki_zaemon <haruki_zaemon@67570413-2614-0410-8c7a-c7689aa8d500>
2006-07-18 12:08:51 +0000

    [NEW] Support validates_numericality_of for numbers (including :integer => true). [NEW] Support validates_length_of for strings.
    git-svn-id: svn://rubyforge.org/var/svn/redhillonrails/trunk/vendor/plugins/schema_validations@36 67570413-2614-0410-8c7a-c7689aa8d500

--- lib/red_hill_consulting/schema_validations.rb
+++ lib/red_hill_consulting/schema_validations.rb
@@ -4,7 +4,15 @@ module RedHillConsulting
       def inherited(child)
         super
         child.content_columns.reject { |column| column.name =~ /(_at|_on)$/ }.each do |column|
-          child.validates_presence_of column.name unless column.null
+          if column.klass == Fixnum
+            child.validates_numericality_of column.name, :allow_nil => column.null, :only_integer => true
+          elsif column.number?
+            child.validates_numericality_of column.name, :allow_nil => column.null
+          elsif column.class == String and column.limit
+            child.validates_length_of column.name, :allow_nil => column.null, :maximum => column.limit
+          elsif !column.null
+            child.validates_presence_of column.name
+          end
         end
       end
     end
