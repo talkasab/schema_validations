b10e7b8eaa4d20d37b514250517e2143326f81df
haruki_zaemon <haruki_zaemon@67570413-2614-0410-8c7a-c7689aa8d500>
2006-07-19 03:20:56 +0000

    [FIXED] String test checks #class instead of #klass [CHANGED] Separated out check for null from validation of numericality. This means you'll get a "xxx can't be blank" rather than an "xx isn't a number" which seems more intuitive to me.
    git-svn-id: svn://rubyforge.org/var/svn/redhillonrails/trunk/vendor/plugins/schema_validations@38 67570413-2614-0410-8c7a-c7689aa8d500

--- lib/red_hill_consulting/schema_validations.rb
+++ lib/red_hill_consulting/schema_validations.rb
@@ -5,14 +5,14 @@ module RedHillConsulting
         super
         child.content_columns.reject { |column| column.name =~ /(_at|_on)$/ }.each do |column|
           if column.klass == Fixnum
-            child.validates_numericality_of column.name, :allow_nil => column.null, :only_integer => true
+            child.validates_numericality_of column.name, :allow_nil => true, :only_integer => true
           elsif column.number?
-            child.validates_numericality_of column.name, :allow_nil => column.null
-          elsif column.class == String and column.limit
-            child.validates_length_of column.name, :allow_nil => column.null, :maximum => column.limit
-          elsif !column.null
-            child.validates_presence_of column.name
+            child.validates_numericality_of column.name, :allow_nil => true
+          elsif column.klass == String && column.limit
+            child.validates_length_of column.name, :allow_nil => true, :maximum => column.limit
           end
+
+          child.validates_presence_of column.name unless column.null || column.default
         end
       end
     end
