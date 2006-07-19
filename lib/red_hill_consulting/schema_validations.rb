35c6b9f80976eb928093e472aa54b943dcc00a41
haruki_zaemon <haruki_zaemon@67570413-2614-0410-8c7a-c7689aa8d500>
2006-07-19 23:56:58 +0000

    [NEW] Support validates_presence_of for belongs_to associations.
    git-svn-id: svn://rubyforge.org/var/svn/redhillonrails/trunk/vendor/plugins/schema_validations@43 67570413-2614-0410-8c7a-c7689aa8d500

--- lib/red_hill_consulting/schema_validations.rb
+++ lib/red_hill_consulting/schema_validations.rb
@@ -1,6 +1,18 @@
 module RedHillConsulting
   module SchemaValidations
     module Base
+      def self.extended(base)
+        class << base
+          alias_method :belongs_to_without_schema_validations, :belongs_to unless method_defined?(:belongs_to_without_schema_validations)
+          alias_method :belongs_to, :belongs_to_with_schema_validations
+        end
+      end
+
+      def belongs_to_with_schema_validations(association_id, options = {})
+        belongs_to_without_schema_validations(association_id, options)
+        validates_presence_of association_id unless columns_hash[reflections[association_id.to_sym].primary_key_name.to_s].null
+      end
+
       def inherited(child)
         super
         child.content_columns.reject { |column| column.name =~ /(_at|_on)$/ }.each do |column|
