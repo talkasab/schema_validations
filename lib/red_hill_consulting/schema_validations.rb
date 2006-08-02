c632c48afa6a30f34b016d5678899b3ba7b6df89
haruki_zaemon <haruki_zaemon@67570413-2614-0410-8c7a-c7689aa8d500>
2006-08-02 01:59:49 +0000

    [NEW] Support for validates_uniqueness_of for single-column unique indexes. [CHANGED] Error message for boolean fields to be the same as the default for validates_presence_of.
    git-svn-id: svn://rubyforge.org/var/svn/redhillonrails/trunk/vendor/plugins/schema_validations@50 67570413-2614-0410-8c7a-c7689aa8d500

--- lib/red_hill_consulting/schema_validations.rb
+++ lib/red_hill_consulting/schema_validations.rb
@@ -10,13 +10,17 @@ module RedHillConsulting
 
       def belongs_to_with_schema_validations(association_id, options = {})
         belongs_to_without_schema_validations(association_id, options)
-        validates_presence_of association_id unless columns_hash[reflections[association_id.to_sym].primary_key_name.to_s].null
+        # validates_presence_of association_id unless columns_hash[reflections[association_id.to_sym].primary_key_name.to_s].null
       end
 
       def inherited(child)
         super
+        
+        # NOT NULL constraints
         child.content_columns.reject { |column| column.name =~ /(_at|_on)$/ }.each do |column|
-          if column.type == :integer
+          if column.name == "position"
+            next
+          elsif column.type == :integer
             child.validates_numericality_of column.name, :allow_nil => true, :only_integer => true
           elsif column.number?
             child.validates_numericality_of column.name, :allow_nil => true
@@ -28,11 +32,16 @@ module RedHillConsulting
             # Work-around for a "feature" of the way validates_presence_of handles boolean fields
             # See http://dev.rubyonrails.org/ticket/5090 and http://dev.rubyonrails.org/ticket/3334
             if column.type == :boolean
-              child.validates_inclusion_of column.name, :in => [true, false]
+              child.validates_inclusion_of column.name, :in => [true, false], :message => ActiveRecord::Errors.default_error_messages[:blank]
             else
               child.validates_presence_of column.name
             end
           end
+        end
+        
+        # Single-column UNIQUE indexes
+        child.connection.indexes(child.table_name, "#{child.name} Indexes").each do |index|
+          child.validates_uniqueness_of index.columns.first if index.unique && index.columns.size == 1
         end
       end
     end
