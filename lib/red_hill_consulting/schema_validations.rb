1aebb7d87985c37f6a8c4fc483ed18cc44ee1db8
haruki_zaemon <haruki_zaemon@67570413-2614-0410-8c7a-c7689aa8d500>
2006-09-09 04:55:15 +0000

    Removed Foreign Key Support and Schema Defining plugins and replaced them with a more general RedHill on Rails Core (redhillonrails_core).
    git-svn-id: svn://rubyforge.org/var/svn/redhillonrails/trunk/vendor/plugins/schema_validations@112 67570413-2614-0410-8c7a-c7689aa8d500

--- lib/red_hill_consulting/schema_validations.rb
+++ lib/red_hill_consulting/schema_validations.rb
@@ -1,27 +1,14 @@
 module RedHillConsulting
   module SchemaValidations
     module Base
-      def self.extended(base)
-        class << base
-          alias_method :belongs_to_without_schema_validations, :belongs_to unless method_defined?(:belongs_to_without_schema_validations)
-          alias_method :belongs_to, :belongs_to_with_schema_validations
-        end
-      end
-
-      def belongs_to_with_schema_validations(association_id, options = {})
-        belongs_to_without_schema_validations(association_id, options)
-        validates_presence_of association_id unless columns_hash[reflections[association_id.to_sym].primary_key_name.to_s].null
-        # TODO: validates_uniqueness_of association_id ...
-      end
-
       def inherited(child)
         super
 
         # Don't even bother if the table doesn't yet exist
         return unless child.table_exists?
-        
+
         # NOT NULL constraints
-        child.content_columns.reject { |column| column.name =~ /^(((created|updated)_(at|on))|position)$/ }.each do |column|
+        child.columns.reject { |column| column.primary || column.name =~ /(^(((created|updated)_(at|on))|position)|_count)$/ }.each do |column|
           if column.type == :integer
             child.validates_numericality_of column.name, :allow_nil => true, :only_integer => true
           elsif column.number?
@@ -29,7 +16,7 @@ module RedHillConsulting
           elsif column.text? && column.limit
             child.validates_length_of column.name, :allow_nil => true, :maximum => column.limit
           end
-          
+
           if !column.null && column.default.blank?
             # Work-around for a "feature" of the way validates_presence_of handles boolean fields
             # See http://dev.rubyonrails.org/ticket/5090 and http://dev.rubyonrails.org/ticket/3334
@@ -40,11 +27,10 @@ module RedHillConsulting
             end
           end
         end
-        
-        # Single-column UNIQUE indexes
-        # TODO: Probably need to ignore _id columns?
-        child.connection.indexes(child.table_name, "#{child.name} Indexes").each do |index|
-          child.validates_uniqueness_of index.columns.first if index.unique && index.columns.size == 1
+
+        # UNIQUE constraints
+        child.columns.reject { |column| column.primary }.each do |column|
+          child.validates_uniqueness_of column.name if column.unique
         end
       end
     end
