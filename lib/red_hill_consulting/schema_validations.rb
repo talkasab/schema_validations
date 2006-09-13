c0386f71e2fd5680c889fb3d73aaab5b568ffb20
haruki_zaemon <haruki_zaemon@67570413-2614-0410-8c7a-c7689aa8d500>
2006-09-13 12:12:54 +0000

    [FIXED] validates_presence_of for associations causes new records to fail.
    git-svn-id: svn://rubyforge.org/var/svn/redhillonrails/trunk/vendor/plugins/schema_validations@125 67570413-2614-0410-8c7a-c7689aa8d500

--- lib/red_hill_consulting/schema_validations.rb
+++ lib/red_hill_consulting/schema_validations.rb
@@ -1,14 +1,35 @@
 module RedHillConsulting
   module SchemaValidations
     module Base
+      def self.extended(base)
+        class << base
+          alias_method :belongs_to_without_schema_validations, :belongs_to unless method_defined?(:belongs_to_without_schema_validations)
+          alias_method :belongs_to, :belongs_to_with_schema_validations
+        end
+      end
+
+      def belongs_to_with_schema_validations(association_id, options = {})
+        belongs_to_without_schema_validations(association_id, options)
+        
+        column = columns_hash[reflections[association_id.to_sym].primary_key_name.to_s]
+
+        # NOT NULL constraints
+        module_eval(
+          "validates_presence_of column.name, :if => lambda { |record| record.#{association_id}.nil? || !record.#{association_id}.new_record? }"
+        ) if column.required
+
+        # UNIQUE constraints
+        validates_uniqueness_of column.name if column.unique
+      end
+
       def inherited(child)
         super
 
         # Don't even bother if the table doesn't yet exist
         return if child.name.blank? || !child.table_exists?
 
-        # NOT NULL constraints
-        child.columns.reject { |column| column.primary || column.name =~ /(^(((created|updated)_(at|on))|position)|_count)$/ }.each do |column|
+        child.content_columns.reject { |column| column.name =~ /^(((created|updated)_(at|on))|position)$/ }.each do |column|
+          # Data-type validation
           if column.type == :integer
             child.validates_numericality_of column.name, :allow_nil => true, :only_integer => true
           elsif column.number?
@@ -17,7 +38,8 @@ module RedHillConsulting
             child.validates_length_of column.name, :allow_nil => true, :maximum => column.limit
           end
 
-          if !column.null && column.default.nil?
+          # NOT NULL constraints
+          if column.required
             # Work-around for a "feature" of the way validates_presence_of handles boolean fields
             # See http://dev.rubyonrails.org/ticket/5090 and http://dev.rubyonrails.org/ticket/3334
             if column.type == :boolean
@@ -26,10 +48,8 @@ module RedHillConsulting
               child.validates_presence_of column.name
             end
           end
-        end
 
-        # UNIQUE constraints
-        child.columns.reject { |column| column.primary }.each do |column|
+          # UNIQUE constraints
           child.validates_uniqueness_of column.name if column.unique
         end
       end
