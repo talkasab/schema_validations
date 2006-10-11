5e3957866f37c04c0d5606b17de6d87fb238b24b
haruki_zaemon <haruki_zaemon@67570413-2614-0410-8c7a-c7689aa8d500>
2006-10-11 11:52:06 +0000

    [CHANGED] Use include rather than extend in init.rb as this is more general. Plugin code can then determine if it needs to use extend or not.
    git-svn-id: svn://rubyforge.org/var/svn/redhillonrails/trunk/vendor/plugins/schema_validations@161 67570413-2614-0410-8c7a-c7689aa8d500

--- lib/red_hill_consulting/schema_validations.rb
+++ lib/red_hill_consulting/schema_validations.rb
@@ -1,56 +1,62 @@
 module RedHillConsulting
   module SchemaValidations
     module Base
-      def self.extended(base)
-        class << base
-          alias_method :belongs_to_without_schema_validations, :belongs_to unless method_defined?(:belongs_to_without_schema_validations)
-          alias_method :belongs_to, :belongs_to_with_schema_validations
-        end
+      def self.included(base)
+        base.extend(ClassMethods)
       end
-
-      def belongs_to_with_schema_validations(association_id, options = {})
-        belongs_to_without_schema_validations(association_id, options)
-        
-        column = columns_hash[reflections[association_id.to_sym].primary_key_name.to_s]
-
-        # NOT NULL constraints
-        module_eval(
-          "validates_presence_of column.name, :if => lambda { |record| record.#{association_id}.nil? }"
-        ) if column.required
-
-        # UNIQUE constraints
-        validates_uniqueness_of column.name, :scope => column.unique_scope, :allow_nil => true if column.unique
-      end
-
-      def inherited(child)
-        super
-
-        # Don't even bother if a table doesn't exist
-        return if child.abstract_class? || child.name.blank? || !child.table_exists?
-
-        child.content_columns.reject { |column| column.name =~ /^(((created|updated)_(at|on))|position)$/ }.each do |column|
-          # Data-type validation
-          if column.type == :integer
-            child.validates_numericality_of column.name, :allow_nil => true, :only_integer => true
-          elsif column.number?
-            child.validates_numericality_of column.name, :allow_nil => true
-          elsif column.text? && column.limit
-            child.validates_length_of column.name, :allow_nil => true, :maximum => column.limit
+      
+      module ClassMethods
+        def self.extended(base)
+          class << base
+            alias_method :belongs_to_without_schema_validations, :belongs_to unless method_defined?(:belongs_to_without_schema_validations)
+            alias_method :belongs_to, :belongs_to_with_schema_validations
           end
+        end
+
+        def belongs_to_with_schema_validations(association_id, options = {})
+          belongs_to_without_schema_validations(association_id, options)
+        
+          column = columns_hash[reflections[association_id.to_sym].primary_key_name.to_s]
 
           # NOT NULL constraints
-          if column.required
-            # Work-around for a "feature" of the way validates_presence_of handles boolean fields
-            # See http://dev.rubyonrails.org/ticket/5090 and http://dev.rubyonrails.org/ticket/3334
-            if column.type == :boolean
-              child.validates_inclusion_of column.name, :in => [true, false], :message => ActiveRecord::Errors.default_error_messages[:blank]
-            else
-              child.validates_presence_of column.name
-            end
-          end
+          module_eval(
+            "validates_presence_of column.name, :if => lambda { |record| record.#{association_id}.nil? }"
+          ) if column.required
 
           # UNIQUE constraints
-          child.validates_uniqueness_of column.name, :scope => column.unique_scope, :allow_nil => true if column.unique
+          validates_uniqueness_of column.name, :scope => column.unique_scope, :allow_nil => true if column.unique
+        end
+
+        def inherited(child)
+          super
+
+          # Don't even bother if a table doesn't exist
+          return if child.abstract_class? || child.name.blank? || !child.table_exists?
+
+          child.content_columns.reject { |column| column.name =~ /^(((created|updated)_(at|on))|position)$/ }.each do |column|
+            # Data-type validation
+            if column.type == :integer
+              child.validates_numericality_of column.name, :allow_nil => true, :only_integer => true
+            elsif column.number?
+              child.validates_numericality_of column.name, :allow_nil => true
+            elsif column.text? && column.limit
+              child.validates_length_of column.name, :allow_nil => true, :maximum => column.limit
+            end
+
+            # NOT NULL constraints
+            if column.required
+              # Work-around for a "feature" of the way validates_presence_of handles boolean fields
+              # See http://dev.rubyonrails.org/ticket/5090 and http://dev.rubyonrails.org/ticket/3334
+              if column.type == :boolean
+                child.validates_inclusion_of column.name, :in => [true, false], :message => ActiveRecord::Errors.default_error_messages[:blank]
+              else
+                child.validates_presence_of column.name
+              end
+            end
+
+            # UNIQUE constraints
+            child.validates_uniqueness_of column.name, :scope => column.unique_scope, :allow_nil => true if column.unique
+          end
         end
       end
     end
