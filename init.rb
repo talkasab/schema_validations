3e3a986aade5fe523ab31d4a0998f98602190279
haruki_zaemon <haruki_zaemon@67570413-2614-0410-8c7a-c7689aa8d500>
2007-11-07 14:04:09 +0000

    [NEW] Force loading of redhillonrails_core as the first step of initialisation. [NEW] Updated to the latest Rails initialisation files.
    git-svn-id: svn://rubyforge.org/var/svn/redhillonrails/trunk/vendor/plugins/schema_validations@304 67570413-2614-0410-8c7a-c7689aa8d500

--- init.rb
+++ init.rb
@@ -1,1 +1,3 @@
+Rails::Plugin::Loader.new(initializer, File.join(directory, '..', 'redhillonrails_core')).load
+
 ActiveRecord::Base.send(:include, RedHillConsulting::SchemaValidations::ActiveRecord::Base)
